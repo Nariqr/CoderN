d6370673aa39a24532f2cb133d058de1b1bd866c
Nariqr <43245274+Nariqr@users.noreply.github.com>
2018-09-28 08:48:59 -0400

    Create codes.js
    personal code

---
+++ codes.js
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title> WINN $5 </title>
+        <style>
+            .paragraph-one{
+                color:rgb(13, 153, 0);
+            }
+            body{
+                background-color:rgb(204, 245, 206);
+            }
+            
+        </style>
+    </head>
+    
+    <body>
+    
+    <h1>Get a free chance to (WIN) $5 </h1>
+    <img src = "https://www.kasandbox.org/programming-images/landscapes/beach-in-hawaii.png"alt="JPG" width = "500" height ="200" >
+    <p class= "paragraph-one "> This page allows you to <strong>Participate</strong> in survey taking for an appourtunity to WIN A <strong>FREE</strong> Cruise to the Bahammas, by simply taking some 50 hour long surveys. This procces might take up to 3 days, so beaware to get enough rest after 5 surveys and for  every <strong>2 surveys</strong> took you also have a Chance to Win a $70 gift card.   </p>
+<h2> 
+    Heres are 6 steps to get you started!!.
+<ul>
+<li> 
+    First, click on the link at the bottom of the page. </li>
+<li>
+    Second, on the screen you will select create account.
+</li>
+<li>
+Third, enter your email address and create your own unique password. </li>
+           <li></li>
+       </ul>
+    </h2>
+    
+        
+    </body>
+</html>
